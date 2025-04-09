3c845794e776cc40b6e38f69015a436986247d97
Pedro Sotelino <pedrosotelino0908@gmail.com>
2025-04-09 00:08:53 -0300

    Main

---
+++ main/urls.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+from . import views
+
+app_name = "main"
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("about", views.about, name="about"),
+    path("perfil", views.perfil, name="perfil"),
+    path("editar-perfil", views.editar_perfil, name="editar_perfil"),
+]
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
---
+++ main/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+# Create your views here.
+from .forms import EditUserForm, AvatarForm
+from .models import Avatar
+
+def index(request):
+    return render(request, "main/index.html")
+
+def about(request):
+    return render(request, "main/about.html")
+
+def perfil(request):
+    return render(request, "main/perfil.html")
+
+
+@login_required
+def editar_perfil(request):
+    if request.method == "POST":
+        form = EditUserForm(request.POST, instance=request.user)
+
+        # Verificar si el usuario tiene un avatar
+        try:
+            avatar = request.user.avatar
+        except Avatar.DoesNotExist:
+            avatar = None
+
+        # Crear el formulario de avatar según si el usuario tiene uno o no
+        if avatar:
+            avatar_form = AvatarForm(request.POST, request.FILES, instance=avatar)
+        else:
+            avatar_form = AvatarForm(request.POST, request.FILES)
+
+        if form.is_valid() and avatar_form.is_valid():
+            form.save()
+            avatar_instance = avatar_form.save(commit=False)
+            avatar_instance.user = request.user  # Asignar el usuario actual al avatar
+            avatar_instance.save()
+            return redirect("main:perfil")
+    else:
+        form = EditUserForm(instance=request.user)
+        if hasattr(request.user, "avatar"):
+            avatar_form = AvatarForm(instance=request.user.avatar)
+        else:
+            avatar_form = AvatarForm()
+    return render(request, "main/editar_perfil.html", {"form" : form, "avatar_form": avatar_form})
