c3c5da9097123f4309a88f3dd16de580ac6e5d50
Pedrosote <pedrosotelino0908@gmail.com>
2025-04-10 18:30:28 -0300

    Delete myapp directory

--- myapp/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def index(request):
-    context = {"mensaje": "q ona bienvenio a la pagina"}
-    return render(request,"myapp/index.html", context)
