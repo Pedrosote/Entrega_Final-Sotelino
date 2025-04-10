ca7d09e94cd6a491933e1807d811e3bd2283bc2a
Pedro Sotelino <pedrosotelino0908@gmail.com>
2025-04-10 17:53:06 -0300

    blog

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+
+class Post(models.Model):
+    imagen = models.ImageField(upload_to='imagenes_post/', null=True, blank=True)
+
+    def __str__(self):
+        return self.titulo
+
 class Post(models.Model):
     class estado(models.TextChoices):
         BORRADOR = "B", "borrador"
@@ -10,5 +17,6 @@ class Post(models.Model):
     fecha = models.DateTimeField(auto_now_add=True)
     autor = models.ForeignKey( User, on_delete=models.CASCADE)
     estado = models.CharField(max_length=1, choices=estado.choices, default = estado.BORRADOR)
+    imagen = models.ImageField(upload_to='posts/', blank=True, null=True)
     def __str__(self):
         return self.titulo
